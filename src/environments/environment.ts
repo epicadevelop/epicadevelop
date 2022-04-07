0dc53802d8e39423399f7a0b1582797a4c0481b1
Marcos <omnia_vincit@msn.com>
2022-04-07 18:32:57 -0300

    Alteração teste

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  serverUrl: 'http://localhost:8080/api'
+//  serverUrl: 'http://localhost:8080/api'
+  serverUrl: 'http://192.168.0.233:8080/api'
 };
 
 /*
