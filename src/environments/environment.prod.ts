bb68e77aa3fd15affa22bf3490ff251721afee8c
Marcos <omnia_vincit@msn.com>
2022-04-12 11:51:28 -0300

    Atualização heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-//serverUrl: 'http://192.168.0.233:8080/api'
-serverUrl: 'http://200.205.213.82:8080/api'
+serverUrl: 'http://192.168.0.233:8080/api'
+//serverUrl: 'http://200.205.213.82:8080/api'
 };
