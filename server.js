f04ef81a18223ce4984e59879bd155da88558bd1
Marcos <omnia_vincit@msn.com>
2022-04-12 17:42:52 -0300

    Tentativa sem o dist

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(`${__dirname}/${nomeApp}`));
 
 console.log("caminho : " , `${__dirname}/${nomeApp}`);
 console.log("caminho curto : " , `${nomeApp}`);
-console.log("POrta : ", process.env.PORT);
+console.log("Porta : ", process.env.PORT);
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+res.sendFile(path.join(`${__dirname}/${nomeApp}/index.html`));
 });
 
 app.listen(process.env.PORT);
