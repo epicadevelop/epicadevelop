875938e3e5fe2099f2021ae2a939e2d8b6e5cdb1
Marcos <omnia_vincit@msn.com>
2022-04-18 18:14:56 -0300

    Novo deploy heruko 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
 //serverUrl: 'http://localhost:8080/api'
-serverUrl: 'https://api._boleto.jardimdacolina.com.br/api'
+serverUrl: 'http://api._boleto.jardimdacolina.com.br:8080/api'
 };
