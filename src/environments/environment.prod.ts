f31b8ce2c91da9c7fe29aad1e436e93db8c681e6
Marcos <omnia_vincit@msn.com>
2022-04-28 20:24:24 -0300

    Atualização 28/04 20:24

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
 //  serverUrl: 'http://192.168.0.233:8080/api'
-serverUrl: 'http://api.jardimdacolina.com.br:8080/api'
+serverUrl: 'https://api.jardimdacolina.com.br:8443/api'
 };
